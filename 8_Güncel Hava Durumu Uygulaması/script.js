796f2eabe8a9fb58f88077b0515e904430d1ada5
yesim <95049839+ysmltr@users.noreply.github.com>
2023-03-14 16:17:32 +0300

    Update script.js

--- 8_Güncel Hava Durumu Uygulaması/script.js
+++ 8_Güncel Hava Durumu Uygulaması/script.js
@@ -3,6 +3,7 @@ const url='https://api.openweathermap.org/data/2.5'
 const key='a3e51a8071394c96b26ef0e5d0fd1895'
 
 
+
 const setQuery=(e)=>{
     if (e.keyCode=='13') {
         getResult(searchBar.value);
@@ -12,17 +13,31 @@ const setQuery=(e)=>{
 
 
 const getResult=(cityName)=>{
-    let query=`${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`
+    let query = `${url}/weather?q=${cityName}&appid=${key}&units=metric&lang=en`;
     fetch(query)
     .then(weather=>{
         return weather.json()
     })
-    .then(displayResult);
+    .then(displayResult)
 }
 
-const displayResult=(result)=>{
-    console.log(result);
+const displayResult = (result)=>{
+    let city=document.querySelector('.city');
+    city.innerText=`${result.name},${result.sys.country}`;
+
+    let temp=document.querySelector('.temp');
+    temp.innerText=`${Math.round(result.main.temp)}°C`;
+
+    let desc=document.querySelector('.desc');
+    desc.innerText= result.weather[0].description
+
+    let minMax=document.querySelector('.minmax');
+    minMax.innerText=`${Math.round(result.main.temp_min)}°C / 
+        ${Math.round(result.main.temp_max)}°C`
+
+
 }
+
 
 
 
